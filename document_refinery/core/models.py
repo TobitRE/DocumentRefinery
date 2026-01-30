30e4bc845ae052e1b1e35e8e6abd3142c508f0e6
Tobit Esch <t.esch@7hn.de>
2026-01-30 23:02:10 +0100

    Add core app and BaseModel

---
+++ document_refinery/core/models.py
@@ -0,0 +1,33 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    Base model, contains all fields models should have
+    """
+
+    uuid = models.UUIDField(
+        unique=True,
+        editable=False,
+        default=uuid.uuid4,
+        verbose_name="Public Identifier",
+    )
+    created_at = models.DateTimeField(
+        "Erstellungsdatum", null=True, blank=True, auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        "Letzte Änderungen", null=True, blank=True, auto_now=True
+    )
+
+    def save(self, *args, **kwargs):
+        skip_full_clean = kwargs.pop("skip_full_clean", False)
+        if not skip_full_clean:
+            self.full_clean()  # Ensures model validation on save
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+# Create your models here.
